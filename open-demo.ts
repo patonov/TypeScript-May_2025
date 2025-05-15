fef648b764b78a2bcb12681d4f060e85b1976870
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-05-15 20:58:53 +0300

    Add files via upload

---
+++ open-demo.ts
@@ -0,0 +1,13 @@
+console.log('hi, TS!')
+
+let str: string = "Muncho";
+
+let decimal_variable: number = 1.333;
+let integer_variable: number = 20;
+let binary_variable: number = 11100010;
+
+let boolean_variable = 5 > 2;
+
+let nums_array = [11, 22, 44, 55]
+
+console.log(nums_array.includes(100))
