f10e037c293a567f720868793a1de36ce4578b3f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-05-16 22:11:25 +0300

    Add files via upload

--- open-demo.ts
+++ open-demo.ts
@@ -8,6 +8,32 @@ let binary_variable: number = 11100010;
 
 let boolean_variable = 5 > 2;
 
-let nums_array = [11, 22, 44, 55]
+let nums_array = [11, 22, 44, 55];
 
-console.log(nums_array.includes(100))
+let one_symbol = Symbol('Yoga');
+let two_symbol = Symbol('Yoga');
+
+console.log(one_symbol === two_symbol); //it would print just false
+
+let firstName : string | undefined;
+
+let skills : string[] = ['JS', 'C#', 'SQL'];
+
+let certificateInfo : [string, number, boolean] = ['The spirit is great', 1001, true];
+
+let num_string_arr : Array<number | string> = ['wine', 'vodka', 'beer', 11]; //(string | number)[]
+
+let point = {x: 0, y: 0};
+
+enum Directions {
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+function movePoint(point: {x: number, y: number}, direct: Directions){
+    if(direct === Directions.Up){
+        return {x: point.x, y: point.y + 1};
+    }
+}
