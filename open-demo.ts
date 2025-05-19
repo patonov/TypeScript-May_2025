d17d8bcc592d3c92585333f3bade9dc3c7bc4d36
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-05-19 22:43:57 +0300

    Add files via upload

--- open-demo.ts
+++ open-demo.ts
@@ -35,5 +35,30 @@ enum Directions {
 function movePoint(point: {x: number, y: number}, direct: Directions){
     if(direct === Directions.Up){
         return {x: point.x, y: point.y + 1};
+    } else if (direct === Directions.Down){
+        return {x: point.x, y: point.y - 1};
+    } else if (direct === Directions.Left){
+        return {x: point.x - 1, y: point.y};
+    } else if (direct === Directions.Right){
+        return {x: point.x + 1, y: point.y};
     }
 }
+
+function greetUser(username: string, addGreet?: boolean) : string {
+    if (addGreet === true){
+        return `Hi, ${username}`;
+    }
+
+    return username;
+}
+
+console.log(greetUser("Nikolay", true));
+
+function formatData(a: string | number, b: string | number) {
+    if(typeof a === 'number' && typeof b === 'string'){
+        console.log(a+b);
+    } else {
+        console.log(`${a} => ${b}`);
+    }
+}
+
