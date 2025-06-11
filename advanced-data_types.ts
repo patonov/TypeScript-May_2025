b5c867ccd4bffb5684e8fe88927d770df94a9715
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-06-11 20:47:05 +0300

    Add files via upload

--- advanced-data_types.ts
+++ advanced-data_types.ts
@@ -141,3 +141,41 @@ function httpCodes(httpCode: httpCode | httpCodeWithPrintChars){
         case 500: console.log(httpCode.text.slice(0, httpCode.printChars)); return;
     }
 }
+
+type gym = {number: 1, train:() => void} & ({hallway: 'A', pass?: 'Guest'} | {hallway: 'C'});
+type restaurant = {number: 2, dine:() => void} & ({hallway: 'A', pass?: 'Guest'} | {hallway: 'C'});
+type hotel = {number: 3, sleep:() => void, hallway: 'A' | 'C'}; 
+type simplified = gym | restaurant | hotel;
+
+// function visitFloor(floor:
+// { number: 1, hallway: 'A', train:() => void, pass: 'Guest' } |
+// { number: 2, hallway: 'A', dine: () => void, } |
+// { number: 3, hallway: 'C', sleep:() => void} |
+// { number: 1, hallway: 'C', train:() => void } |
+// { number: 1, hallway: 'A', train:() => void } |
+// { number: 2, hallway: 'A', dine: () => void, pass: 'Guest' } |
+// { number: 3, hallway: 'A', sleep:() => void } |
+// { number: 2, hallway: 'C', dine: () => void, }
+// ) {
+//     switch (floor.number) {
+//     case 1: floor.train(); return;
+//     case 2: floor.dine(); return;
+//     case 3: floor.sleep(); return;
+//     }
+// }
+
+function visitFloor(floor: simplified) {
+    switch (floor.number) {
+    case 1: floor.train(); return;
+    case 2: floor.dine(); return;
+    case 3: floor.sleep(); return;
+    }
+}
+
+visitFloor({ train() { }, number: 1, hallway: 'A', pass: 'Guest' });
+visitFloor({ dine() { }, number: 2, hallway: 'A' });
+visitFloor({ sleep() { }, number: 3, hallway: 'C' });
+visitFloor({ train() { }, number: 1, hallway: 'C' });
+visitFloor({ train() { }, number: 1, hallway: 'A' });
+visitFloor({ dine() { }, number: 2, hallway: 'A', pass: 'Guest' });
+visitFloor({ sleep() { }, number: 3, hallway: 'A' }); visitFloor({ dine() { }, number: 2, hallway: 'C' });
