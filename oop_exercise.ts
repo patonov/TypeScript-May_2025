c5eda03a1f94ea53ff4dc9ade237008d4ac1d120
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-06-22 20:17:59 +0300

    Add files via upload

--- oop_exercise.ts
+++ oop_exercise.ts
@@ -13,3 +13,50 @@ class demoPerson{
         return `I am ${this.firstName} ${this.lastName} and I am ${this.age} years old.`;
     }
 }
+
+class BankAccount{
+    private balance: number;
+
+    constructor(balance: number){
+        this.balance = balance;
+    }
+
+    public deposit(amount: number): void {
+        this.balance += amount;
+    }
+
+    public withdraw(amount: number): void {
+        if(this.balance >= amount){
+            this.balance -= amount;
+        }
+    }
+
+    public getBalance(): number {
+        return this.balance;
+    }
+}
+
+class Vehicle{
+    public brand: string;
+
+    constructor(brand: string){
+        this.brand = brand;
+    }
+
+    public drive(): string{
+        return `${this.brand} is driving.`;
+    }
+}
+
+class Car extends Vehicle {
+    public model: string;
+
+    constructor(brand: string, model: string){
+        super(brand);
+        this.model = model;
+    }
+
+    public override drive(): string {
+        return `${this.brand} ${this.model} is driving.`;
+    }
+}
