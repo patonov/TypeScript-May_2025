4aac83d8896548faa8ef1077baaa205a3a8a3c18
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-07-09 18:47:31 +0300

    Add files via upload

--- generics_lab.ts
+++ generics_lab.ts
@@ -4,3 +4,10 @@ function getFirstParameter<T>(arr: T[]): T {
 
 const unit = getFirstParameter<number>([33, 22, 16]);
 console.log(unit);
+
+function makeTuple<T, U>(el1: T, el2: U): [T, U] {
+    return [el1, el2];
+}
+
+const tupleMade = makeTuple<string, number>("pencho", 1);
+console.log(tupleMade);
