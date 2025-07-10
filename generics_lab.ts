2d6008194ebffe390bd9b75f086f86e7cbb03860
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-07-10 18:44:09 +0300

    Add files via upload

--- generics_lab.ts
+++ generics_lab.ts
@@ -11,3 +11,60 @@ function makeTuple<T, U>(el1: T, el2: U): [T, U] {
 
 const tupleMade = makeTuple<string, number>("pencho", 1);
 console.log(tupleMade);
+
+interface Messaage<T> {
+    sender: string;
+    recipient: string;
+    data: T;
+};
+
+const messageOne: Messaage<string> = {
+    sender: "Muncho",
+    recipient: "Wuyu",
+    data: "call me on my cell phone!",
+};
+
+const messageTwo: Messaage<{text: string, timestamp: number}> = {
+    sender: 'Waiyu',
+    recipient: 'Yuyin',
+    data: {text: 'bim bam bum', timestamp: 1001001}
+};
+
+
+function logItemId<T extends { id: number }>(item: T): void {
+    console.log(item);
+}
+
+logItemId({id: 1, text: 'hahha'});
+
+class StorageBox<T> {
+    items: T[] = [];
+
+    constructor(initialItems: T[]){
+        this.items = initialItems;
+    }
+
+    getAll(): T[] {
+        return this.items;
+    }
+
+    getFirstElement(): T {
+        return this.items[0];
+    }
+
+    add(item: T): void {
+        this.items.push(item);
+    }
+
+    reverse(): void {
+        this.items.reverse();
+    }
+
+    removeItem(item: T): void {
+        const index = this.items.indexOf(item);
+
+        if (index > -1){
+            this.items.splice(index, 1);
+        }
+    }
+}
